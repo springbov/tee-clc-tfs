d108f1dc933dedf85e446228b15a29b2781cdca0
Ivan Gabriele <ivan.gabriele@gmail.com>
2016-01-19 23:27:58 +0100

    first TFS run

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var chalk  = require('chalk'),
     fs     = require('fs'),
     output = require('./lib/utils/output'),
+    SETTINGS,
     setup  = require('./lib/setup')(__dirname),
     spawn  = require('child_process').spawn;
 
@@ -8,31 +9,25 @@ function verbose(data) {
   process.stdout.write(chalk.gray(data.toString()));
 }
 
-// %ProgramFiles(x86)%
 function tfsRun(tfsOptions, callback) {
-  tfsOptions.forEach(function(option) {
-    console.log(option, spawn);
-  });
-
-  try {
-    var batch = spawn('dir');
-  }
-  catch (exception) {
-    console.log(exception);
-  }
+  var spawn = require('child_process').spawn,
+      batch = spawn(SETTINGS.tfPath, tfsOptions);
 
   batch.stdout.on('data', output.log);
   batch.stderr.on('data', output.error);
-  batch.on('exit', callback);
+
+  if (callback) {
+    batch.on('exit', callback);
+  }
 }
 
-/*tfsRun(['get'], function() {
-  console.log(123);
-});*/
 
 function setupAndStart() {
-  setup();
-  // test();
+  SETTINGS = setup();
+
+  tfsRun(['get', '/recursive'], function() {
+    console.log(123);
+  });
 }
 
 setupAndStart();
