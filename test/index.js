73bc544f4aeeec563d8b0259f492a6104864b908
Ivan Gabriele <ivan.gabriele@gmail.com>
2016-01-22 00:18:08 +0100

    TFS exposer file + Refactoring + Mocha tests + Debug mode

--- test/index.js
+++ test/index.js
@@ -1,9 +1,48 @@
-var assert = require('assert');
-describe('Array', function() {
-  describe('#indexOf()', function () {
-    it('should return -1 when the value is not present', function () {
-      assert.equal(-1, [1,2,3].indexOf(5));
-      assert.equal(-1, [1,2,3].indexOf(0));
+var assert = require('assert'),
+    debug  = require('../lib/utils/debug'),
+    tfs    = require('../lib/tfs');
+
+function path(path) {
+  return path.substr(3).replace(/\\/g, '/');
+}
+
+function cwd() {
+  return path(process.cwd());
+}
+
+function itemsFile() {
+  return cwd() + '/..';
+}
+
+describe('TFS Tests Suite', function() {
+
+  before(function() {
+    console.log('\n    > Switching debugging environment mode ON\n');
+    debug(true);
+  });
+
+  after(function() {
+    console.log('\n    > Switching debugging environment mode OFF');
+    debug(false);
+  });
+
+  var commands = [
+    'checkin',
+    'get',
+    'undo'
+  ];
+
+  commands.forEach(function(command) {
+    describe(command.toUpperCase(), function() {
+      it('SHOULD use CWD when [item] is NOT specified', function () {
+        assert.equal('get "' + cwd() + '"', tfs('get'));
+      });
+      it('SHOULD use ONE ITEM when 1 [items] IS specified', function () {
+        assert.equal('get "' + itemsFile() + '"', tfs('get', itemsFile()));
+      });
+      it('SHOULD use TWO ITEMS when 2 [items] ARE specified', function () {
+        assert.equal('get "' + itemsFile() + '" "' + itemsFile() + '"', tfs('get', itemsFile() + ' ' + itemsFile()));
+      });
     });
   });
 });
