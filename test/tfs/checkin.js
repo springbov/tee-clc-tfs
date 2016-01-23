911d03cf076053bac0a70886d8aa378838ea5ea4
Ivan Gabriele <ivan.gabriele@gmail.com>
2016-01-23 21:28:23 +0100

    Checkin options

---
+++ test/tfs/checkin.js
@@ -0,0 +1,35 @@
+var assert = require('assert'),
+    debug  = require('../../lib/utils/debug'),
+    tfs    = require('../../lib/tfs');
+
+describe('CHECKIN Tests Suite', function() {
+  var options = {
+    b: [
+      'bypass',
+      'force',
+      'noprompt',
+      'recursive',
+      'saved',
+      'validate'
+    ],
+    s: [
+      'author',
+      'collection',
+      'comment',
+      'login',
+      'notes',
+      'override',
+      'shelveset'
+    ]
+  };
+
+  it('SHOULD works with BOOLEAN options', function () {
+    var test = debug.getBooleanAssert(options);
+    assert.equal('checkin "' + debug.cwd(true) + '" ' + test.output, tfs('checkin', null, test.options));
+  });
+
+  it('SHOULD works with STRING options', function () {
+    var test = debug.getStringAssert(options);
+    assert.equal('checkin "' + debug.cwd(true) + '" ' + test.output, tfs('checkin', null, test.options));
+  });
+});
