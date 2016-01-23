c8dd2e349cc5e735db5d501acc5eb3a0325452e2
Ivan Gabriele <ivan.gabriele@gmail.com>
2016-01-23 15:06:04 +0100

    + Git pre-commit hook & refactoring 12 commands

--- gruntfile.js
+++ gruntfile.js
@@ -2,6 +2,13 @@ module.exports = function(grunt) {
   var libFiles = 'lib/**/*.js';
 
   grunt.initConfig({
+    githooks: {
+      all: {
+        'pre-commit': {
+          taskNames: 'test doc'
+        }
+      }
+    },
     jsdoc: {
       dist: {
         src: libFiles,
@@ -17,12 +24,20 @@ module.exports = function(grunt) {
         },
         target: libFiles
       }
+    },
+    mochaTest: {
+      dist: {
+        src: 'test/**/*.js'
+      }
     }
   });
 
   grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-githooks');
   grunt.loadNpmTasks('grunt-jsdoc');
+  grunt.loadNpmTasks('grunt-mocha-test');
 
   grunt.registerTask('doc', ['jshint:dist', 'jsdoc:dist']);
   grunt.registerTask('lint', 'jshint:dist');
+  grunt.registerTask('test', 'mochaTest:dist');
 };
